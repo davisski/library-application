3508a839f9dc81984d98af7827b751a64abc2c62
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-06-28 15:02:47 +0300

    refactor code using setters and getters

--- Book.js
+++ Book.js
@@ -3,8 +3,21 @@ class Book {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
-    this.out = false;
     this.dueDate = null;
     this.patron = null;
+    this._out = false;
+  }
+  set out(out) {
+    this._out = out;
+    if (this._out) {
+      const newDueDate = new Date();
+      newDueDate.setDate(newDueDate.getDate() + 14);
+      book.dueDate = newDueDate;
+    } else {
+      this.dueDate = null;
+    }
+  }
+  get out() {
+    return this._out;
   }
 }
